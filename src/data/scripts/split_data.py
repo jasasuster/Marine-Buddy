042c3c90b2eb02a11c7e55bec82de310849e8d2d
jasasuster <92645266+jasasuster@users.noreply.github.com>
2024-05-29 07:59:46 +0200

    Fix split data script

--- src/data/scripts/split_data.py
+++ src/data/scripts/split_data.py
@@ -11,11 +11,11 @@ def split_data(df):
 def main():
   data_path = "data/processed/"
   for sea_point_number in range(1, 11):
-    file_path = os.path.join(data_path, f"{sea_point_number}.csv")
+    file_path = os.path.join(data_path, f"sea_point_{sea_point_number}.csv")
 
     df = pd.read_csv(file_path)
 
     train_df, test_df = split_data(df)
 
-    train_df.to_csv(f'data/processed/{sea_point_number}_train.csv', index=False)
-    test_df.to_csv(f'data/processed/{sea_point_number}_test.csv', index=False)
+    train_df.to_csv(f'data/processed/sea_point_{sea_point_number}_train.csv', index=False)
+    test_df.to_csv(f'data/processed/sea_point_{sea_point_number}_test.csv', index=False)
